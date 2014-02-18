6b2c7ae2f484215e1e83cebf3d644600d657cb58
Romain Prieto <choicesmade@gmail.com>
2014-02-19 10:30:49 +1100

    Fix discovery of modules in "lib"
    `./lib` was only working when running local examples, we need `__dirname` when this is used as an NPM module.

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ var changeCase  = require('change-case');
 // my-module  ->  streams.myModule
 //
 
-var files = fs.readdirSync('./lib');
+var files = fs.readdirSync(path.join(__dirname, 'lib'));
 var streams = files.reduce(function(memo, filename) {
   var name = path.basename(filename, '.js');
-  var fullPath = path.resolve(path.join('.', 'lib', filename));
+  var fullPath = path.resolve(path.join(__dirname, 'lib', filename));
   var key = changeCase.camelCase(name);
   memo[key] = require(fullPath);
   return memo;
