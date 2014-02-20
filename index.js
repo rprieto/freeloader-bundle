0dce7688f1f94ed92b1c68342201112163a62a48
Romain Prieto <romain.prieto@gmail.com>
2014-02-21 09:14:04 +1100

    Fix loader path to "lib"

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ var changeCase  = require('change-case');
 // my-module  ->  streams.myModule
 //
 
-var files = wrench.readdirSyncRecursive('lib');
+var files = wrench.readdirSyncRecursive(path.join(__dirname, 'lib'));
 
 var streams = files.reduce(function(memo, filepath) {
   if (filepath.match(/\.js$/)) {
     var name = path.basename(filepath, '.js');
-    var fullpath = path.resolve(path.join('lib', filepath));
+    var fullpath = path.resolve(path.join(__dirname, 'lib', filepath));
     var key = changeCase.camelCase(name);
     memo[key] = require(fullpath);
   }
